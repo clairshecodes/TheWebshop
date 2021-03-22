d09359c6eaff03de37db494c3c7f2dc1f9355dba
Clair <clairshecodes@gmail.com>
2021-03-22 13:35:57 +0100

    Components setup
    All components are set

---
+++ my-webshop/src/app/authentication/log-in/log-in.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styles: [
+  ]
+})
+export class LogInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
