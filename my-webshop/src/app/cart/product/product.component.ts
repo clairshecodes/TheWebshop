3ac55e892d10e6d5438a07a1e25f66fad958b67f
Clair <clairshecodes@gmail.com>
2021-03-29 09:02:55 +0200

    changes
    changes

--- my-webshop/src/app/cart/product/product.component.ts
+++ my-webshop/src/app/cart/product/product.component.ts
@@ -9,6 +9,7 @@ export class ProductComponent implements OnInit {
   myImage:string = "assets/image/sweatshirt.jpg";
   priceRangeFrom = '50kr';
   priceRangeTo = '5000kr';
+  pricing = "something kr.";
   constructor() { }
   
 
@@ -19,5 +20,9 @@ export class ProductComponent implements OnInit {
 }
 onUpdatePriceTo(event: any){
   this.priceRangeTo = (event.target as HTMLInputElement).value;
+}
+onUpdatePricing(event: any){
+  this.pricing = (event.target as HTMLInputElement).value;
+
 }
 }
