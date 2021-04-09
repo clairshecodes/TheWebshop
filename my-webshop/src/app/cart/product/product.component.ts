47e1e362d87fad203953fbbb599ed31ef2d4590a
haida <Haidariano@hotmail.com>
2021-04-09 13:38:59 +0200

    nothing changed

--- my-webshop/src/app/cart/product/product.component.ts
+++ my-webshop/src/app/cart/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from './product.model';
 
 @Component({
@@ -7,28 +7,31 @@ import {Product} from './product.model';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  myImage:string = "assets/image/sweatshirt.jpg";
+  myImage: string = "assets/image/sweatshirt.jpg";
   priceRangeFrom = '50kr';
   priceRangeTo = '5000kr';
   pricing = "something kr.";
   products: Product[] = [
     new Product('Sweatshirty', 'So this is a warm sweatshirt and good at winter'
-    , 'https://www.kids-world.dk/images/417px/JX179.jpg' )
+      , 'https://www.kids-world.dk/images/417px/JX179.jpg')
   ];
 
-  constructor() { }
-  
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-  onUpdatePriceFrom(event: any){
-    this.priceRangeFrom = (event.target as HTMLInputElement).value;
-}
-onUpdatePriceTo(event: any){
-  this.priceRangeTo = (event.target as HTMLInputElement).value;
-}
-onUpdatePricing(event: any){
-  this.pricing = (event.target as HTMLInputElement).value;
 
-}
+  onUpdatePriceFrom(event: any) {
+    this.priceRangeFrom = (event.target as HTMLInputElement).value;
+  }
+
+  onUpdatePriceTo(event: any) {
+    this.priceRangeTo = (event.target as HTMLInputElement).value;
+  }
+
+  onUpdatePricing(event: any) {
+    this.pricing = (event.target as HTMLInputElement).value;
+
+  }
 }
