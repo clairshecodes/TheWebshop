0636dc396740da33d73524060be2d7548449210e
Mohammad Javad Zamani <s176485@win.dtu.dk>
2021-04-27 14:26:34 +0200

    commit

--- my-webshop/src/app/app-routing.module.ts
+++ my-webshop/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
-import {LogInComponent} from './authentication/log-in/log-in.component';
-import {MainComponent} from './main/main.component';
-import {SignUpComponent} from './authentication/sign-up/sign-up.component';
 
+import {MainComponent} from './main/main.component';
+import {CheckoutComponent} from './checkout/checkout.component';
+import {CartComponent} from './cart/cart.component';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {LogInComponent} from './authentication/log-in/log-in.component';
+import {SignUpComponent} from './authentication/sign-up/sign-up.component';
 
 const appRoutes: Routes = [
   {path: '', component: MainComponent},
   {path: 'login', component: LogInComponent},
   {path: 'signUp', component: SignUpComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'authentication', component: AuthenticationComponent},
+  {path: 'main', component: MainComponent},
+  {path: 'login', component: LogInComponent}
 
 ];
 
--- my-webshop/src/app/app.module.ts
+++ my-webshop/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import {HeaderComponent} from './header/header.component'
 import {AppComponent} from './app.component';
@@ -32,14 +32,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
 
-const appRoutes: Routes = [
-  {path: 'checkout', component: CheckoutComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'authentication', component: AuthenticationComponent},
-  {path: 'main', component: MainComponent},
-  {path: 'login', component: LogInComponent}
 
-];
 
 
 @NgModule({
