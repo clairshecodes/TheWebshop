631965337af1fc1a7e5712774683ba144f88825c
Clair <clairshecodes@gmail.com>
2021-05-03 20:05:43 +0200

    log in
    log in it is

--- my-webshop/src/app/authentication/log-in/log-in.component.ts
+++ my-webshop/src/app/authentication/log-in/log-in.component.ts
@@ -1,31 +1,58 @@
-import {Component, OnInit} from '@angular/core';
-import { NgForm } from '@angular/forms'
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styles: []
+  styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-  username: string;
-  password: string;
-  userId : string;
-  
 
-  constructor() { 
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
+
+  // in here we initialize our form
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
+      }),
+      gender: new FormControl('male')
+    });
+   
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    );
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    this.signupForm.reset();
 
   }
 
-  ngOnInit(): void {
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 
-  loginUser(form :NgForm) {
-    if(form.invalid)
-        {return}
-    const email = form.value.email
-    const password = form.value.password
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
 
   }
- 
 }
