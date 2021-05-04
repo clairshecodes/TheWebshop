96f321a9dd42cbeb7782f6c3d53ca6f8739b2c75
haida <Haidariano@hotmail.com>
2021-05-04 16:20:16 +0200

    Virker ikke.

--- my-webshop/src/app/authentication/log-in/log-in.component.ts
+++ my-webshop/src/app/authentication/log-in/log-in.component.ts
@@ -1,44 +1,11 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+export class LogInComponent {
+  userName = "";
+  passWord = "";
 
-
-@Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in-new.component.html',
-  styleUrls: ['./log-in.component.css']
-})
-export class LogInComponent implements OnInit {
-
-  genders = ['male', 'female'];
-  signupForm: FormGroup;
-  forbiddenUsernames = ['Chris', 'Anna'];
-
-
-  // in here we initialize our form
-  ngOnInit() {
-    this.fetchPosts();
-  }
-
-  constructor(private http: HttpClient) {}
-  onSubmit(postData: { title: string; content: string }) {
-    // Send Http request
-    this.http
-      .post(
-        'https://webshop-dtu-default-rtdb.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
-  }
-
-  private fetchPosts(){
-    this.http.get('https://webshop-dtu-default-rtdb.firebaseio.com/posts.json')
-    .subscribe(posts => {
-      console.log(posts);
-    });
+  onAddPost(){
+    const logIn : LogIn = {
+      userName: this.userName,
+      passWord: this.passWord
+    };
   }
 }
-
---
+++ my-webshop/src/app/authentication/log-in/login.model.ts
@@ -0,0 +1,4 @@
+export class interface {
+  userName: string;
+  passWord: string;
+}
