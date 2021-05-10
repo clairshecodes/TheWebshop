3697ddd62d8c468a62b2c826a8dabf94b6a543a7
Clair <clairshecodes@gmail.com>
2021-05-10 10:08:47 +0200

    confirmation done
    confirmation with order information is done

--- my-webshop/src/app/authentication/log-in/log.component.ts
+++ my-webshop/src/app/authentication/log-in/log.component.ts
@@ -2,71 +2,15 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {any} from 'codelyzer/util/function';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-log-in',
-  templateUrl: './log-in-new.component.html',
+  templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
 export class LogInComponent implements OnInit {
-  // ViewChild gives us access to the Form.
-//  @ViewChild('f', {static: false}) sgnForm: NgForm;
-  /*defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  /!*In order to only ouput only some specifik data,
-  we add this property which is JsO. This are not related to our Form.
-  When the submit button is pushed we want to update data *!/
-  user: {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
-
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // Allows you to set the values of the whole form. We need to pass javascript object exactly representing our form.
-    // setValue command return a exact copy of the Form value, but this is not the best approach. Override the values of each control.
-    /!*  this.signupForm.setValue({
-        userData: {
-          username: suggestedName,
-          email: ''
-        },
-        secret: 'pet',
-        questionsAnswer: '',
-        gender: 'male'
-
-      });*!/
-
-    // by passing java script object as values in patchValue method, you override only (a specific) controls.
-    this.signupForm.form.patchValue({
-      userData: {
-        userName: suggestedName
-      }
-    });
-  }
-
-  /!*onSubmit(form: NgForm){
-    console.log(form);
-  }*!/
-
-  onSubmit() {
-    this.submitted = true;
-    //console.log(this.signupForm); // ViewChild, is very useful, if you need to access the form not just at the time you submitted but also earlier.
-    // we access our form by signupForm and fetching the data from the html code to set this data equal to the JsO property user*
-    /!* this.user.username = this.signupForm.value.username;
-     this.user.email = this.signupForm.value.email;
-     this.user.secretQuestion = this.signupForm.value.secret;
-     this.user.answer = this.signupForm.value.questionAnswer;
-     this.user.gender = this.signupForm.value.gender;
-     this.signupForm.reset();*!/
-    console.log(this.sgnForm.value);
-  }*/
   genders = ['male', 'female'];
   signupForm: FormGroup;
   forbiddenUsernames = ['Chris', 'Anna'];
@@ -80,10 +24,6 @@ export class LogInComponent implements OnInit {
       }),
       gender: new FormControl('male')
     });
-    //A Form state you can track in generel. Valuechanges is an observable. Everytime form changes it ouput every single change.
-    /* this.signupForm.valueChanges.subscribe(
-       (value) => console.log(value)
-     );*/
 
     //Status changes, with this we can view states that changes. Nice observable, allowing you to see whant is happening in your form.
     this.signupForm.statusChanges.subscribe(
@@ -118,5 +58,23 @@ export class LogInComponent implements OnInit {
     });
     return promise;
 
+  }
+  constructor(private http: HttpClient) {}
+  Submit(postData) {
+    this.http
+      .post(
+        'https://webshop-dtu-default-rtdb.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  private fetchPosts(){
+    this.http.get('https://webshop-dtu-default-rtdb.firebaseio.com/posts.json')
+    .subscribe(posts => {
+      console.log(posts);
+    });
   }
 }
