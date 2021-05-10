fd7abada4bc4886cd51006d71ba167b930220f00
Asim Raja <asim.raja11@gmail.com>
2021-05-10 20:26:04 +0200

    authentication

--- my-webshop/backend/app.js
+++ my-webshop/backend/app.js
@@ -16,8 +16,9 @@ mongoose.connect(
     console.log("Connection failed!");
   });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
---
+++ my-webshop/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: "http://Backendfrontend-env-1.eba-f67xpvfa.us-east-2.elasticbeanstalk.com/api"
+};
